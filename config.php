11cc6569c32138b556daff210c2f6392312d0e88
Arnaldo Martins Hidalgo Junior <53869033+hidalgojunior@users.noreply.github.com>
2025-04-23 09:55:48 -0300

    Config.php create

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	$nome = "Mercado YAHE";
+
+
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$base = "yahe_mercado";
+
+	$con = mysqli_connect($servidor, $usuario, $senha, $base) or die("Erro ao conectar a base de dados");
+
+?>
